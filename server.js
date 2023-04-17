c96bf41dab24aab7886ab7e24c08d7a80e16a796
Haozhe-H <haozhehuang@outlook.com>
2023-04-17 17:41:36 -0400

    update route back to main and stored notes

--- server.js
+++ server.js
@@ -11,11 +11,28 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // route to notes page
-app.get('/notes', (req, res)=>{
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-})
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+// route to home page
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+// route to read /db/db.json and return stored data
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+
+//
+
+
+//
+
 
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT}`)
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
