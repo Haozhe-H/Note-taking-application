48432eb071536dce4a0e5b072d51f636bbe6cb88
Haozhe-H <haozhehuang@outlook.com>
2023-04-17 17:28:44 -0400

    update and tested route to notes page

--- server.js
+++ server.js
@@ -9,3 +9,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+// route to notes page
+app.get('/notes', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+})
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
