201b056de57ee5636a65a8cc03c41760295435be
Haozhe-H <haozhehuang@outlook.com>
2023-04-21 20:54:40 -0400

    finished debugging

--- server.js
+++ server.js
@@ -16,58 +16,62 @@ app.get("/notes", (req, res) => {
 });
 
 // route to home page
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./public/index.html"));
+// });
 
 // route to read /db/db.json and return stored data
 app.get("/api/notes", (req, res) => {
-  fs.readFile(path.join(__dirname, "./db/db.json"), 'utf-8', (err, data)=>{
-    if(err) throw err
-    res.json(JSON.parse(data))
+  fs.readFile(path.join(__dirname, "./db/db.json"), "utf-8", (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data));
   });
 });
 
 // path.join(__dirname, "/db/db.json")
 // post route
 app.post("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    if (err) throw err;
+    var notes = JSON.parse(data);
+    let newNotes = {
+      title: req.body.title,
+      text: req.body.text,
+      id: Math.floor(Math.random() * 1000),
+    };
+    notes.push(newNotes);
 
-    fs.readFile('./db/db.json', 'utf-8', (err, data)=>{
-        if(err) throw err
-        var notes = JSON.parse(data)
-        let newNotes = {
-            title: req.body.title,
-            text: req.body.text,
-            id: Math.floor(Math.random() * 1000),
-        };
-        notes.push(newNotes)
-
-        fs.writeFile('./db/db.json', JSON.stringify(notes, null, 2), (err, data)=>{
-            if(err) throw err
-            res.json(newNotes)
-        })
-    })
+    fs.writeFile(
+      "./db/db.json",
+      JSON.stringify(notes, null, 2),
+      (err, data) => {
+        if (err) throw err;
+        res.json(newNotes);
+      }
+    );
+  });
   //   console.log(notes);
 });
 
 // delete route
-app.delete('/api/notes/:id', (req, res)=>{
-
-    let notesId = req.params.id
-    fs.readFile("./db/db.json", 'utf-8', (err, data)=>{
-        if(err) throw err
-        let notes = JSON.parse(data)
-
-        let deletedNotes = notes.filter(note => note.id !== notesId)
-        fs.writeFile("./db/db.json", JSON.stringify(deletedNotes), (err)=>{
-            if(err) console.log(err);
-        });
-
-    })
-
-    res.end()
-
-})
+app.delete("/api/notes/:id", (req, res) => {
+  // console.log(req.params.id);
+  let notesId = req.params.id;
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    if (err) throw err;
+    let notes = JSON.parse(data);
+    // console.log(notes);
+    let deletedNotes = notes.filter((note) => note.id != notesId);
+    fs.writeFile(
+      "./db/db.json",
+      JSON.stringify(deletedNotes, null, 2),
+      (err) => {
+        if (err) console.log(err);
+        res.json(deletedNotes);
+      }
+    );
+  });
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
